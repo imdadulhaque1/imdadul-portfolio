b06a32274eb33bc90353458fa5dc1b60b1c1044d
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-02 17:43:15 +0600

    Navbar Created and Customized

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from "../src/components/Home";
 import About from "../src/components/About";
 import Contact from "../src/components/Contact";
 import Service from "../src/components/Service";
-import { Switch, Route} from "react-router-dom";
+import Navbar from "../src/components/Header/Navbar";
+import { Switch, Route, Redirect} from "react-router-dom";
 
 const App = () =>{
   return(
     <>
+    <Navbar />
       <Switch>
         <Route exact path="/" component= {Home} />
         <Route exact path="/about" component= {About} />
         <Route exact path="/service" component= {Service} />
-        <Route exact path="/contact" component= {Contact} />
+        <Route exact  path="/contact" component= {Contact} />
+        <Redirect to="/" />
       </Switch>
     </>
   );
